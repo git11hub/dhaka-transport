19e6e1011237012392508a930f4e0d0fdba80962
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-03-20 14:22:25 +0600

    destination without dynamic image

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
@@ -7,6 +7,18 @@ import { useForm } from 'react-hook-form';
 import './Login.css';
 
 const Login = () => {
+    //new line
+    const [newUser, setNewUser] = useState(false);
+    const [user, setUser] = useState({
+        isSignedIn: false,
+        name: '',
+        email: '',
+        password: '',
+        photo: ''
+    });
+    //new line
+
+
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     if (firebase.apps.length === 0) {
         firebase.initializeApp(firebaseConfig);
@@ -31,21 +43,66 @@ const Login = () => {
                 var credential = error.credential;
                 // ...
             });
+    }
 
+    // Github Sign In
+
+    const handleGitHubSignIn = () => {        
+        var githubProvider = new firebase.auth.GithubAuthProvider();
+        firebase
+            .auth()
+            .signInWithPopup(githubProvider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+
+                // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+                var token = credential.accessToken;
+
+                // The signed-in user info.
+                var user = result.user;
+                console.log(user);
+                setLoggedInUser(user);
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
     }
 
 
     const { register, handleSubmit, watch, errors } = useForm();
     const onSubmit = data => {
         console.log('form submitted', data);
+
+        //new line
+        firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+            .then((userCredential) => {
+                // Signed in 
+                var user = userCredential.user;
+                setUser(user);
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ..
+            });
+        // data.preventDefault();
+        //new line
+
     }
 
     console.log(watch("example")); // watch input value by passing the name of it
     return (
 
         < form className="login-form" onSubmit={handleSubmit(onSubmit)} >
-            {/* < input name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your Name" />
-            { errors.name && <span className="error">Name is required</span>} */}
 
             < input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your Email" />
             { errors.email && <span className="error">Email is required</span>}
@@ -53,12 +110,13 @@ const Login = () => {
             < input type="password" name="password" ref={register({ required: true })} placeholder="Password" />
             { errors.password && <span className="error">Password is required</span>}
 
-            {/* < input name="phone" ref={register({ required: true })} placeholder="Your Phone no." />
-            { errors.phone && <span className="error">Phone number is required</span>} */}
+            <input type="submit" value="Submit" />
 
-            <input type="submit" />
-            <br/>
+            <br />
             <button onClick={handleGoogleSignIn}>Google Sign In</button>
+            <br/>
+            <br/>
+            <button onClick={handleGitHubSignIn}>Git Hub Sign In</button>
         </form >
 
 
--- src/components/OrderRide/OrderRide.js
+++ src/components/OrderRide/OrderRide.js
@@ -1,9 +1,10 @@
+import { Button, Card, Container, ListGroup } from 'react-bootstrap';
 import React, { useEffect, useState } from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
 
 const OrderRide = () => {
-    const {destination} = useParams();
+    const { destination } = useParams();
     console.log(destination);
 
 
@@ -19,16 +20,52 @@ const OrderRide = () => {
     return (
         <Row>
             <Col xs={6} md={4}>
-                <h3>Uttara to Farmgate</h3>
                 <Row>
-                    <Col><img src="" alt=""/></Col>
+                    <Form.Group>
+                        <Form.Control size="lg" type="text" placeholder="Pick From" />
+                        <br />
+                        <Form.Control size="lg" type="text" placeholder="Pick To" />
+                        <br />
+                        <Button variant="danger" size="lg" block>Search</Button>
+                    </Form.Group>
+                </Row>
+
+
+                <Row className="mt-5">
+                <h3>Uttara to Farmgate:</h3>
+                    <Row className="mt-4">
+                        <Col md={3}><img className="w-50" src="https://i.ibb.co/4PMVywf/Frame.png" alt="" /></Col>
+                        <Col md={2}><h4>{destination}</h4></Col>
+                        <Col md={2}><h4>2</h4></Col>
+                        <Col md={2}><h4>$100</h4></Col>
+                    </Row>
+                    <Row>
+                        <Col md={3}><img className="w-50" src="https://i.ibb.co/4PMVywf/Frame.png" alt="" /></Col>
+                        <Col md={2}><h4>{destination}</h4></Col>
+                        <Col md={2}><h4>2</h4></Col>
+                        <Col md={2}><h4>$100</h4></Col>
+                    </Row>
+                    <Row>
+                        <Col md={3}><img className="w-50" src="https://i.ibb.co/4PMVywf/Frame.png" alt="" /></Col>
+                        <Col md={2}><h4>{destination}</h4></Col>
+                        <Col md={2}><h4>2</h4></Col>
+                        <Col md={2}><h4>$100</h4></Col>
+                    </Row>
+
+
+
+                    {/* <h3>Uttara to Farmgate</h3>
+                    <Col><img src="" alt="" /></Col>
                     <Col><h4>{destination}</h4></Col>
                     <Col><h4>2</h4></Col>
-                    <Col><h4>$100</h4></Col>
+                    <Col><h4>$100</h4></Col> */}
                 </Row>
+                
+
+
             </Col>
             <Col xs={6} md={4}>
-                <img src="https://i.ibb.co/2cd3CM8/Map.png" alt=""/>
+                <img src="https://i.ibb.co/2cd3CM8/Map.png" alt="" />
             </Col>
         </Row>
         // <div>
